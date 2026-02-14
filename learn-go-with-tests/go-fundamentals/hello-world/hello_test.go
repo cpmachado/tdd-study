20e5b6c7a34ecb7da7b506a690c412076e6227b6
cpmachado <cpmachado@protonmail.com>
2026-02-14 22:53:40 +0000

    learn-go-with-tests/go-fundamentals/hello-world: Hello, world... again

--- learn-go-with-tests/go-fundamentals/hello-world/hello_test.go
+++ learn-go-with-tests/go-fundamentals/hello-world/hello_test.go
@@ -3,14 +3,33 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	// given
-	name := "cpmachado"
-	want := "hello, " + name
+	t.Run("saying hello to people", func(t *testing.T) {
+		// given
+		name := "cpmachado"
+		want := "hello, " + name
 
-	// when
-	got := Hello(name)
+		// when
+		got := Hello(name)
 
-	// then
+		// then
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying 'hello, world' when an empty string is supplied", func(t *testing.T) {
+		// given
+		name := ""
+		want := "hello, world"
+
+		// when
+		got := Hello(name)
+
+		// then
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
