fb361dac7c90f8867e493725b6ede358ecd7dca6
cpmachado <cpmachado@protonmail.com>
2026-02-14 23:21:00 +0000

    lgwt/gf/integers: first tests

---
+++ lgwt/gf/integers/adder_test.go
@@ -0,0 +1,35 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	t.Run("Add(2, 2) should be 4", func(t *testing.T) {
+		// given
+		a, b := 2, 2
+		expected := 4
+
+		// when
+		sum := Add(a, b)
+
+		// then
+		assertCorrectAdd(t, sum, expected)
+	})
+	t.Run("Add(1, 1) should be 2", func(t *testing.T) {
+		// given
+		a, b := 1, 1
+		expected := 2
+
+		// when
+		sum := Add(a, b)
+
+		// then
+		assertCorrectAdd(t, sum, expected)
+	})
+}
+
+func assertCorrectAdd(t testing.TB, sum, expected int) {
+	t.Helper()
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
