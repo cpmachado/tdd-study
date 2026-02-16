98cf10a94251668e07146ca43780f2910308c0a1
cpmachado <cpmachado@protonmail.com>
2026-02-16 22:15:00 +0000

    lgwt/gf/arrays: generalize Sum

--- lgwt/gf/arrays/sum_test.go
+++ lgwt/gf/arrays/sum_test.go
@@ -3,15 +3,31 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	// given
-	numbers := [5]int{1, 2, 3, 4, 5}
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		// given
+		numbers := []int{1, 2, 3, 4, 5}
+		want := 15
 
-	// when
-	got := Sum(numbers)
+		// when
+		got := Sum(numbers)
 
-	// then
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		// then
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		// given
+		numbers := []int{1, 2, 3}
+		want := 6
+
+		// when
+		got := Sum(numbers)
+
+		// then
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
